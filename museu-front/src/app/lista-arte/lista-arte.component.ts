0ff485cb38d389fcae99ee6f2d67b3bafdba5dd3
silas.reis <85849377+SilasReisUSP@users.noreply.github.com>
2022-07-19 21:22:49 -0300

    correcao filtros

--- museu-front/src/app/lista-arte/lista-arte.component.ts
+++ museu-front/src/app/lista-arte/lista-arte.component.ts
@@ -22,16 +22,9 @@ export class ListaArteComponent implements OnInit {
     'cultura',
     'tipo',
     'estilo',
-    'colecao',
-    'dataCompra',
   ];
-  //Variavel utilizada para paginacao
-  paginaAtual = 1
   //Armazenamento da lista de leiloes
   arteList: Arte[] = []
-
-  arteListFiltrado: Arte[] = []
-  anos: number[] = []
   
   constructor(private routes: Router, private arteService: ArteService) { }
 
@@ -48,22 +41,10 @@ export class ListaArteComponent implements OnInit {
         custo: data.custo,
         tipo: data.tipo
     }))   
-    //todos os produtos que estao em data sao passados para leilaoList, que renderiza no html
     this.arteList = data
-    console.log(this.arteList);
-
-    // this.arteList.forEach((e) => {
-    //     if(!this.tipoArteList.includes(e.tipo))
-    //       this.tipoArteList.push(e.tipo)      
-    // })
-
-    // this.arteListFiltrado = this.arteList
   }
 
   ngOnInit(): void {
-
-    for(let i = 2022; i >=1700; i--)
-    this.anos.push(i)
 
     this.filtrosForm = new FormGroup({
         tipo: new FormControl(''),
@@ -79,33 +60,16 @@ export class ListaArteComponent implements OnInit {
   }
 
   filtrar() {
-    console.log('this.arteListFiltrado', this.arteList)
-    this.arteListFiltrado = this.arteList
-    console.log('this.arteListFiltrado', this.arteListFiltrado)
-    this.filtraTipoArte()
-    this.filtrarClasse()
-  }
-
-  filtraTipoArte() {
-    if(this.filtrosForm.get('tipo').value != "") {
-        const filter = { type: this.filtrosForm.get('tipo').value };
-        this.arteService.getArtes(filter)
+    let filter = {type: "", _class: ""};
+    if(this.filtrosForm.get('tipo').value != "") 
+      filter.type = this.filtrosForm.get('tipo').value;
+    if(this.filtrosForm.get('classe').value != "") 
+      filter._class = this.filtrosForm.get('classe').value ;
+    this.arteService.getArtes(filter)
             .subscribe(rst => {
                     this.populateArteList(rst);
         });
-    }    
   }
-
-  filtrarClasse() {
-    if(this.filtrosForm.get('classe').value != "") {
-      const filter = { _class: this.filtrosForm.get('classe').value };
-        this.arteService.getArtes(filter)
-            .subscribe(rst => {
-                    this.populateArteList(rst);
-        });    
-    }
-  }
-
 
   get tipo() { return this.filtrosForm.get('tipo') }
   get classe() { return this.filtrosForm.get('classe') }
